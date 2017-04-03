aca09c283ca046f61fd0e5cb88da7e269745ecad
Ranajit Banerjee <ranajit.banerjee@fusioncharts.com>
2017-04-03 14:25:37 +0530

    Change babel config

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const StandardPeriodSelector = require('./standard-period-selector');
     env.StandardPeriodSelector = factory(env, true);
   }
 })(typeof window !== 'undefined' ? window : this, function (_window, windowExists) {
-  var FC = _window.FusionCharts;
+  var FC = require('fusioncharts');
   FC.register('extension', ['private', 'standard-period-selector', function () {
     FC.registerComponent('extensions', 'standard-period-selector', StandardPeriodSelector({FusionCharts: FC}));
   }]);
